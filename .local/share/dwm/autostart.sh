cb4be2dcb741ed54a8e426929331b21788f3481b
FewCM <sweewei.tham@gmail.com>
2021-12-18 06:32:09 +0800

    update

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -81,10 +81,10 @@ start greenclip daemon &
 xset -dpms &
 start ibus-daemon -drxR
 
-#start mpd &
+start mpd &
 #start mpDris2 &
 start playerctld daemon &
-spotifyd --no-daemon  --autoplay &
+#potifyd --no-daemon  --autoplay &
 #start xrandr --output HDMI-0 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DVI-D-0 --mode 1920x1080 --pos 2560x0 --rotate normal &
 #start xrandr --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal --output DVI-D-1 --mode 1920x1080 --pos 2560x0 --rotate normal &
 
