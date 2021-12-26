692e6e9dcbd34d96a18a9a81c45bf5fde667e976
FewCM <sweewei.tham@gmail.com>
2021-12-26 11:19:15 +0800

    update

--- .local/bin/backup-music.sh
+++ .local/bin/backup-music.sh
@@ -2,3 +2,4 @@
 
 # script to backup Music directory to external hdd
 rsync -aAXHv --info=progress2 /home/fewcm/Music/* /run/media/fewcm/Volume/Music
+rsync -aAXHv --info=progress2 /home/fewcm/Git/* /run/media/fewcm/Volume/Git
